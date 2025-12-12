01cc84ea5365af165a8259ff8432cb3a25d4e3d5
DrGenie <111873566+DrGenie@users.noreply.github.com>
2025-12-12 12:54:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* ===================================================
-   STEPS FETP India Decision Aid
+   STEPS FETP India Decision Aidd
    Next generation script with working tooltips,
    WTP based benefits, sensitivity, Copilot integration and exports
    =================================================== */
